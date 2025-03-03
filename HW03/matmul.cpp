f27b0889938855e281b9223ddf80f3e43037def5
Rajesh Srivatsav Suresh <rsuresh6@tux-109.cae.wisc.edu>
2025-03-02 18:32:19 -0600

    More changes ; Added Q3

--- HW03/matmul.cpp
+++ HW03/matmul.cpp
@@ -4,12 +4,15 @@
 
 #include <cstddef>
 #include <vector>
+#include <omp.h>
 
 void mmul(const float* A, const float* B, float* C, const std::size_t n){
+    #pragma omp parallel for
     for(size_t i=0;i<n;i++){
         for(size_t k=0;k<n;k++){
             for(size_t j=0;j<n;j++){
                 C[i*n+j] += A[i*n+k]*B[k*n+j];
+                // Equivalent to C[i][j] += A[i][k]*B[k][j] 
             }
         }
     }
