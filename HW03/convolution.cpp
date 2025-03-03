7284ff39083ef0c8ee26f0fc8c444d68733f8159
Rajesh Srivatsav Suresh <rsuresh6@tux-121.cae.wisc.edu>
2025-03-03 15:39:38 -0600

    Fixes in conv

--- HW03/convolution.cpp
+++ HW03/convolution.cpp
@@ -7,24 +7,26 @@
 #include <omp.h>
 
 void convolve(const float *image, float *output, std::size_t n, const float *mask, std::size_t m){
-    size_t i_p=0;
-    size_t j_p=0;
     #pragma omp parallel for
     for(size_t x=0;x<n;x++){
         for(size_t y=0;y<n;y++){
+            float res = 0.0f;
+            size_t i_p=0;
+            size_t j_p=0;
             for(size_t i=0;i<m;i++){
                 for(size_t j=0;j<m;j++){
                     i_p = (x+i-((m-1)/2));
                     j_p = (y+j-((m-1)/2));
                     if(i_p <n && i_p >=0 && j_p <n && j_p >= 0)
-                        output[x*n+y] += mask[i*m+j]*image[i_p*n +j_p];
+                        res += mask[i*m+j]*image[i_p*n +j_p];
                     else if(( (i_p <n && i_p >=0) && !(j_p <n && j_p >= 0)) || 
                         (!(i_p <n && i_p >=0) && (j_p <n && j_p >= 0)))
-                        output[x*n+y] += mask[i*m+j]; // f[i,j] is 1 if one of the i, j <0
+                        res += mask[i*m+j]; // f[i,j] is 1 if one of the i, j <0
                     // else 
                     //     f[i,j] = 0;                    
                 }
             }
+            output[x*n+y] = res;
         }
     }
 
